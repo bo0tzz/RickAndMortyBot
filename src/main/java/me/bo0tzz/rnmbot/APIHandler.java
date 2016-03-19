6851a1ace27c21050595ab31c1013c395ea92413
bo0tzz <btdewilligen@gmail.com>
2016-03-20 00:52:34 +0100

    Refactor project to use org.json by command of senpaii zack

--- src/main/java/me/bo0tzz/rnmbot/APIHandler.java
+++ src/main/java/me/bo0tzz/rnmbot/APIHandler.java
@@ -1,14 +1,12 @@
 package me.bo0tzz.rnmbot;
 
-import com.google.gson.Gson;
-
-import com.google.gson.JsonSyntaxException;
-import com.google.gson.reflect.TypeToken;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-
-import java.util.Collections;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by boet on 17-3-2016.
@@ -17,41 +15,39 @@ public class APIHandler {
     private static final String SEARCH_URL = "https://rm.kmp.pw/api/search?q=";
     private static final String RANDOM_URL = "https://rm.kmp.pw/api/random";
 
-    public static List<GIFResult> getResults(String query) {
-        Gson gson = new Gson();
-        String response;
+    public static JSONArray getResults(String query) {
+        HttpResponse<JsonNode> response;
         try {
-            response = Unirest.get(SEARCH_URL).queryString("q", query).asString().getBody();
-        } catch (UnirestException e) {
-            e.printStackTrace();
-            return Collections.EMPTY_LIST;
-        }
-
-        List<GIFResult> results;
-        try {
-            results = gson.fromJson(response, new TypeToken<List<GIFResult>>() {}.getType());
-        } catch (JsonSyntaxException e) {
-            System.out.println("API returned bad response!");
-            e.printStackTrace();
-            return Collections.EMPTY_LIST;
-        }
-        return results;
-    }
-
-    public static GIFResult getRandomGIF() {
-        Gson gson = new Gson();
-        String response;
-        try {
-            response = Unirest.get(RANDOM_URL).asString().getBody();
+            response = Unirest.get(SEARCH_URL).queryString("q", query).asJson();
         } catch (UnirestException e) {
             e.printStackTrace();
             return null;
         }
 
-        GIFResult result;
+        JSONArray results;
         try {
-            result = gson.fromJson(response, GIFResult.class);
-        } catch (JsonSyntaxException e) {
+            results = new JSONArray(response.getBody());
+        } catch (JSONException e) {
+            System.out.println("API returned bad response!");
+            e.printStackTrace();
+            return null;
+        }
+        return results;
+    }
+
+    public static JSONObject getRandomGIF() {
+        HttpResponse<JsonNode> response;
+        try {
+            response = Unirest.get(RANDOM_URL).asJson();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        JSONObject result;
+        try {
+            result = new JSONObject(response.getBody());
+        } catch (JSONException e) {
             System.out.println("API returned bad response!");
             e.printStackTrace();
             return null;
--- src/main/java/me/bo0tzz/rnmbot/RnMListener.java
+++ src/main/java/me/bo0tzz/rnmbot/RnMListener.java
@@ -1,5 +1,8 @@
 package me.bo0tzz.rnmbot;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import pro.zackpollard.telegrambot.api.chat.inline.send.InlineQueryResponse;
 import pro.zackpollard.telegrambot.api.chat.inline.send.results.InlineQueryResult;
 import pro.zackpollard.telegrambot.api.chat.inline.send.results.InlineQueryResultGif;
@@ -34,16 +37,16 @@ public class RnMListener implements Listener {
 
     public void onInlineQueryReceived(InlineQueryReceivedEvent event) {
         List<InlineQueryResult> queryResults = new ArrayList<>();
-        List<GIFResult> gifResults = APIHandler.getResults(event.getQuery().getQuery());
+        JSONArray gifResults = APIHandler.getResults(event.getQuery().getQuery());
         if (gifResults == null) {
             return;
         }
 
-        for (GIFResult gif : gifResults) {
+        for (int i = 0; i < gifResults.length(); i++) {
             try {
                 queryResults.add(InlineQueryResultGif.builder()
-                        .gifUrl(new URL(gif.getUrl()))
-                        .caption(gif.getText())
+                        .gifUrl(new URL(gifResults.getJSONObject(i).getString("url")))
+                        .caption(gifResults.getJSONObject(i).getString("text"))
                         .build()
                 );
             } catch (MalformedURLException e) {
@@ -68,43 +71,42 @@ public class RnMListener implements Listener {
             return;
         }
 
-        GIFResult result;
+        JSONObject result;
         try {
-            result = APIHandler.getResults(event.getArgsString()).get(0);
-        } catch (IndexOutOfBoundsException e) {
+            result = APIHandler.getResults(event.getArgsString()).getJSONObject(0);
+        } catch (JSONException e) {
             event.getChat().sendMessage("Something went wrong while getting the gif! If this happens again, contact the bot maintainer at @bo0tzz.", main.getTelegramBot());
             return;
-        }
-
-        if (result == null) {
+        } catch (NullPointerException e) {
             event.getChat().sendMessage("Couldn't find a gif! Try again with a different term.", main.getTelegramBot());
             return;
         }
+
         InputFile gif;
         try {
-            gif = new InputFile(new URL(result.getUrl()));
+            gif = new InputFile(new URL(result.getString("url")));
         } catch (MalformedURLException e) {
             e.printStackTrace();
             event.getChat().sendMessage("Something went wrong while getting the gif! If this happens again, contact the bot maintainer at @bo0tzz.", main.getTelegramBot());
             return;
         }
-        event.getChat().sendMessage(SendableVideoMessage.builder().video(gif).caption(result.getText()).build(), main.getTelegramBot());
+        event.getChat().sendMessage(SendableVideoMessage.builder().video(gif).caption(result.getString("text")).build(), main.getTelegramBot());
     }
 
     public void randomGIF(CommandMessageReceivedEvent event) {
-        GIFResult result = APIHandler.getRandomGIF();
+        JSONObject result = APIHandler.getRandomGIF();
         if (result == null) {
             event.getChat().sendMessage("Something went wrong while getting the gif! If this happens again, contact the bot maintainer at @bo0tzz.", main.getTelegramBot());
             return;
         }
         InputFile gif;
         try {
-            gif = new InputFile(new URL(result.getUrl()));
+            gif = new InputFile(new URL(result.getString("url")));
         } catch (MalformedURLException e) {
             e.printStackTrace();
             event.getChat().sendMessage("Something went wrong while getting the gif! If this happens again, contact the bot maintainer at @bo0tzz.", main.getTelegramBot());
             return;
         }
-        event.getChat().sendMessage(SendableVideoMessage.builder().video(gif).caption(result.getText()).build(), main.getTelegramBot());
+        event.getChat().sendMessage(SendableVideoMessage.builder().video(gif).caption(result.getString("url")).build(), main.getTelegramBot());
     }
 }
