5027a99936186924050d724a7d03c05e21396154
bo0tzz <btdewilligen@gmail.com>
2016-03-26 20:43:03 +0100

    Debugging

--- src/main/java/me/bo0tzz/rnmbot/RnMListener.java
+++ src/main/java/me/bo0tzz/rnmbot/RnMListener.java
@@ -54,6 +54,7 @@ public class RnMListener implements Listener {
                         .build()
                 );
             } catch (MalformedURLException e) {
+                System.out.println("Error on JSONObject: " + gifResults.getJSONObject(i));
                 e.printStackTrace();
             }
         }
